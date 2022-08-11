11b888e248d275a442579afde7c80fe1a931d553
DESKTOP-TDELPQA\Furkan TURAL <furkantural3328@hotmail.com>
2022-08-11 01:34:11 -0700

    idle and runner cam is created , state drive camera is set up

---
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using Cinemachine;
+using UnityEngine;
+
+namespace Managers
+{
+    public class CameraManager : MonoBehaviour
+    {
+
+        #region Self Variables
+
+        #region Private Variables
+
+        private CinemachineVirtualCamera _RunnerCam;
+        private CinemachineVirtualCamera _IdleCam;
+        private Animator _animator;
+
+        #endregion
+
+        #endregion
+
+        private void Awake()
+        {
+            _RunnerCam = transform.GetChild(0).GetComponent<CinemachineVirtualCamera>();
+            _RunnerCam = transform.GetChild(1).GetComponent<CinemachineVirtualCamera>();
+            _animator = GetComponent<Animator>();
+        }
+    }
+}
