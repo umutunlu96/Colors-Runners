52a0fcabbce1d76b0bfea21c6fde40452a5437d4
DESKTOP-TDELPQA\Furkan TURAL <furkantural3328@hotmail.com>
2022-08-11 12:51:30 -0700

    data creating for lerping

---
+++ Assets/Scripts/Data/UnityObject/CD_Lerp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ValueObject;
+using UnityEngine;
+
+namespace UnityObject
+{
+    [CreateAssetMenu(fileName = "CD_Lerp", menuName = "ColorsRunners/CD_Lerp", order = 0)]
+    public class CD_Lerp : ScriptableObject
+    {
+        public LerpData Data = new LerpData();
+    }
+}
---
+++ Assets/Scripts/Data/ValueObject/LerpData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ValueObject
+{
+    [Serializable]
+    public class LerpData
+    {
+        public float ScaleOffset = 1.3f;
+        public float DistanceOffSet = 1.7f;
+        public Vector3 LerpSpeeds = new Vector3(1, 1, 1);
+    }
+}
