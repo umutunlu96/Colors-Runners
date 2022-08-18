5c5ad7dba42606b903d040ee13c2e74da1f1e758
DESKTOP-TDELPQA\Furkan TURAL <furkantural3328@hotmail.com>
2022-08-18 05:28:39 -0700

    Set Alpha extetion implamented for matarials

---
+++ Assets/Scripts/Extentions/UtilityExtention.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Extentions
+{
+    public static class UtilityExtention
+    {
+        public static void SetAlpha(this Material material, float alpha)
+        {
+            Color _color = material.color;
+            _color.a = alpha;
+            material.color = _color;
+        }
+    }
+}
