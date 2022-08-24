d9182ea9d8b40caf39e3bdbfd76eaccf5f2f7cb1
umutunlu96 <umutt.unlu.96@gmail.com>
2022-08-24 18:04:15 +0300

    Game state change added to player manager

--- Assets/Scripts/Umut/DenemeUmut.cs
+++ Assets/Scripts/Umut/DenemeUmut.cs
@@ -9,41 +9,41 @@ namespace Umut
 {
     public class DenemeUmut : MonoBehaviour
     {
-    public void Runner()
-    {
-        CoreGameSignals.Instance.onChangeGameState?.Invoke(GameStates.Runner);
-        PlayerSignals.Instance.onTranslateAnimationState?.Invoke(new RunnerAnimationState());
-    }
-    
-    public void Idle()
-    {
-        CoreGameSignals.Instance.onChangeGameState?.Invoke(GameStates.Idle);
-        PlayerSignals.Instance.onTranslateAnimationState?.Invoke(new IdleAnimationState());
-    }
-    
-    public void Sneak()
-    {
-        PlayerSignals.Instance.onTranslateAnimationState?.Invoke(new SneakIdleAnimationState());
-    }
-    
-    public void SneakWalk()
-    {
-        PlayerSignals.Instance.onTranslateAnimationState?.Invoke(new SneakWalkAnimationState());
-    }
-    
-    public void Die()
-    {
-        PlayerSignals.Instance.onTranslateAnimationState?.Invoke(new DeathAnimationState());
-    }
-    
-    public void Stop()
-    { 
-        //write player stop signals
-    }
-    
-    public void OnPlay()
-    {
-        CoreGameSignals.Instance.onPlay?.Invoke();
-    }
+    // public void Runner()
+    // {
+    //     CoreGameSignals.Instance.onChangeGameState?.Invoke(GameStates.Runner);
+    //     PlayerSignals.Instance.onTranslateAnimationState?.Invoke(new RunnerAnimationState());
+    // }
+    //
+    // public void Idle()
+    // {
+    //     CoreGameSignals.Instance.onChangeGameState?.Invoke(GameStates.Idle);
+    //     PlayerSignals.Instance.onTranslateAnimationState?.Invoke(new IdleAnimationState());
+    // }
+    //
+    // public void Sneak()
+    // {
+    //     PlayerSignals.Instance.onTranslateAnimationState?.Invoke(new SneakIdleAnimationState());
+    // }
+    //
+    // public void SneakWalk()
+    // {
+    //     PlayerSignals.Instance.onTranslateAnimationState?.Invoke(new SneakWalkAnimationState());
+    // }
+    //
+    // public void Die()
+    // {
+    //     PlayerSignals.Instance.onTranslateAnimationState?.Invoke(new DeathAnimationState());
+    // }
+    //
+    // public void Stop()
+    // { 
+    //     //write player stop signals
+    // }
+    //
+    // public void OnPlay()
+    // {
+    //     CoreGameSignals.Instance.onPlay?.Invoke();
+    // }
     }
 }
